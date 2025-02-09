835876aef2b2c93aec1243b4107cceb2f9e7a65b
Jason Holden <jason@JASONSPC22-Ubuntu-WSL>
2025-02-09 08:14:53 -0700

    - Add cleanup code to startup

--- ping/build.sh
+++ ping/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Stop the application container and cleanup
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=cos-data-and-technology-preso-ping-app))
+docker rmi cos-data-and-technology-preso-ping-app --force
+
 # Build the docker image
 docker build --no-cache -t cos-data-and-technology-preso-ping-app .
