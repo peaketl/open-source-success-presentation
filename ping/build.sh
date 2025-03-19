3afe8881e6fce2950a29e8705ebfc31f383bbf12
Jason Holden <jason@jasonholden.com>
2025-03-19 13:52:14 -0600

    - Delete unnecessary file

--- ping/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop the application container and cleanup
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=cos-data-and-technology-preso-ping-app))
-docker rmi cos-data-and-technology-preso-ping-app --force
-
-# Build the docker image
-docker build --no-cache -t cos-data-and-technology-preso-ping-app .
